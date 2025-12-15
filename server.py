8e2988efa45f7cad3f2e84a5a34841e82266d98f
Anisa Faza <149240692+AnisaFaza@users.noreply.github.com>
2025-12-15 23:35:42 +0700

    Set server port to 5000 explicitly
    Removed dynamic port assignment and set port to 5000.

--- server.py
+++ server.py
@@ -56,9 +56,5 @@ def compare():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-import os
-
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Railway akan set PORT secara otomatis
-    app.run(host="0.0.0.0", port=port)
-
+    app.run(host="0.0.0.0", port=5000)
